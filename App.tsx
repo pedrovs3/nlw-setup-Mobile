7fb83b47905b9980e5673a4882af84e21dd9085d
Pedro <pedrovs3@hotmail.com>
2023-01-17 13:46:36 -0300

    Splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  useFonts
+} from '@expo-google-fonts/montserrat'
+
+import {Loading} from "./src/components/Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded] = useFonts({
+		Montserrat_400Regular,
+		Montserrat_600SemiBold,
+		Montserrat_700Bold,
+		Montserrat_800ExtraBold
+	});
+
+	if (!fontsLoaded) {
+		return (
+				<Loading/>
+		);
+	}
+
+	return (
+			<View style={styles.container}>
+				<Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+				<StatusBar barStyle="light-content" backgroundColor={'transparent'} translucent/>
+			</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#09090A',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontFamily: 'Montserrat_600SemiBold',
+		color: 'red',
+	}
 });
